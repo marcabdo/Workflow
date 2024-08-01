9fe8d5a358b50653474cfc5579f077cfd53e47bd
marcphilipabdo <marcphilipabdo@hotmail.com>
2024-08-01 00:00:19 -0400

    implemented Routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
-
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Application from "./component/applicant/application";
+import ApprovalProcess from "./component/approval1/approvalProcess";
 import React, { useState, useEffect } from "react";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -9,12 +11,12 @@ import "@fontsource/roboto/700.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <div>
-        <Application />
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Application />} />
+        <Route path="/21" element={<ApprovalProcess />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/component/approval1/approvalProcess.js
+++ src/component/approval1/approvalProcess.js
@@ -1,3 +1,13 @@
 /**
  *
  */
+import React, { Component } from "react";
+
+const ApprovalProcess = () => {
+  return (
+    <div>
+      <h1>Approval Process</h1>
+    </div>
+  );
+};
+export default ApprovalProcess;
